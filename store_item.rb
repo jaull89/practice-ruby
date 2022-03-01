24ca6fd47aa0384392f31c0aaa8f51c9b0328c85
Joe Aull <jaull89@gmail.com>
2022-03-01 13:36:19 -0600

    create file store_item

---
+++ store_item.rb
@@ -0,0 +1,11 @@
+apple = {color: "red", type: "food", price: 2}
+
+grapes = {color: "green", type: "food", price: 3}
+
+paper = {:color => "white", :type => "supplies", :price => 5}
+
+p "At our store apples are #{apple[:color]}, classified as a #{apple[:type]} and sell for $#{apple[:price]} each."
+
+p "At our store grapes are #{grapes[:color]}, classified as a #{grapes[:type]} and sell for $#{grapes[:price]} each."
+
+p "At our store paper is #{paper[:color]}, classified as a #{paper[:type]} and sell for $#{paper[:price]} each."
